0419d8872104605292eb568babfddb3724e42fcb
Maíra Marini <maira.marini@gmail.com>
2019-04-04 21:30:49 +0100

    started first view

--- hackathon/src/main/java/org/academiadecodigo/asciimos/controller/HouseController.java
+++ hackathon/src/main/java/org/academiadecodigo/asciimos/controller/HouseController.java
@@ -1,7 +1,5 @@
 package org.academiadecodigo.asciimos.controller;
 
-import org.academiadecodigo.asciimos.HouseDto;
-import org.academiadecodigo.asciimos.model.House;
 import org.academiadecodigo.asciimos.service.HouseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -11,7 +9,6 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.validation.Valid;
 
